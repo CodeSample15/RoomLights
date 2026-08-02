81443f5f9356e6d934cd967d08fb8bd7715f2f68
CodeSample15 <lukecrimi15@gmail.com>
2026-08-02 16:34:46 -0400

    test

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
@@ -9,5 +11,18 @@ const (
 )
 
 func main() {
-	
+	opt := ws2811.DefaultOptions
+	opt.Channels[0].LedCount = ledCounts
+
+	dev, err := ws2811.MakeWS2811(&opt)
+	if err != nil {
+		fmt.Println("Unable to create strip object")
+		return
+	}
+
+	defer dev.Fini()
+
+	color := uint32(0x0000ff)
+	dev.Leds(0)[0] = color
+	dev.Render()
 }
