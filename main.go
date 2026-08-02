b48785673712f603a23b69ef5d93d028012c7578
CodeSample15 <lukecrimi15@gmail.com>
2026-08-02 17:25:28 -0400

    mqtt test

--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
 const (
+	mqttBroker = "100.93.66.64:1883"
+	clientID   = "RoomLightsNode"
+	topic      = "pico_commands"
+
 	ledCounts = 300
 )
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected to MQTT Broker")
+}
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Println("Recieved message: %s", string(msg.Payload()))
+}
 
 func main() {
 	opt := ws2811.DefaultOptions
@@ -26,8 +42,35 @@ func main() {
 	dev.Init()
 	defer dev.Fini()
 
-	color := uint32(0x0000ff)
+	color := uint32(0x000000)
 	dev.Leds(0)[0] = color
 	dev.Render()
 	fmt.Println("done")
+
+	//mqtt test code
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(mqttBroker)
+	opts.SetClientID(clientID)
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	// Subscribe to the topic
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s\n", topic)
+
+	// Wait for interrupt signal to gracefully shutdown the subscriber
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+
+	// Unsubscribe and disconnect
+	fmt.Println("Unsubscribing and disconnecting...")
+	client.Unsubscribe(topic)
+	client.Disconnect(250)
 }
